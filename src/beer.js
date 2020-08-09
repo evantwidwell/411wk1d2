2e67a8268e100dfd6bc736b3e00752511b27f7b0
evantwidwell <evanski@gmail.com>
2020-08-09 07:05:45 -0500

    styled

--- src/beer.js
+++ src/beer.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import './App.css';
 
 const BeerComponent = (props) => {
 return (
 <div className="App">
 
 <div>
-<img src={props.beer.image_url}/>
+<img class = "beerImg" src={props.beer.image_url}/>
 <h1>Beer: {props.beer.name}</h1>
 <h3>{props.beer.tagline}</h3>
 <p>{props.beer.description}</p>
 <button onClick={()=>props.liked(props.beer)}>{props.button}</button>
+<p></p>
 </div>
 
 </div>
--- src/liked.js
+++ src/liked.js
@@ -3,7 +3,7 @@ import React from 'react';
 const LikeComponent = (props) => {
 return (
   <div>
-  {/* <h1>This is Our Listing Component</h1> */}
+ 
  {props.beers.map((item, index)=>{
    console.log(index)
    return <div><h1 key={index}>{props.beers[index].name} </h1>
